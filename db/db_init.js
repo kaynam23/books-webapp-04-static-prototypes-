9e30ef10ea40a9d6ab5505188d8ea315defe8540
Kaylee <kaynam23@bergen.org>
2022-04-28 15:37:46 -0400

    env file

---
+++ db/db_init.js
@@ -0,0 +1,42 @@
+const db = require("./db_connection");
+
+const drop_book_table_sql = "DROP TABLE IF EXISTS `book`;"
+
+db.execute(drop_book_table_sql);
+
+const create_book_table_sql = `
+    CREATE TABLE book (
+        id INT NOT NULL AUTO_INCREMENT,
+        title VARCHAR(45) NOT NULL,
+        author VARCHAR(45) NOT NULL,
+        description VARCHAR(150) NULL,
+        rating INT NULL,
+        PRIMARY KEY (id));
+`
+
+db.execute(create_book_table_sql);
+
+const insert_book_table_sql = `
+    INSERT INTO book
+        (title, author, description, rating)
+    VALUES
+        (?, ?, ?, ?)
+`
+
+db.execute(insert_book_table_sql, ['The Song of Achilles', 'Madeline Miller', 'Set during the Greek Heroic Age, it is an adaptation of Homer\'s Iliad as told from the perspective of Patroclus.', '8']);
+db.execute(insert_book_table_sql, ['If We Were Villians', 'M.L. Rio', null, null]);
+
+const read_stuff_table_sql = "SELECT * FROM book";
+
+db.execute(read_stuff_table_sql,
+    (error, results) => {
+        if (error)
+            throw error;
+        
+        console.log("Table 'book' initialized with: ")
+        console.log(results);
+    }
+);
+
+db.end();
+
---
+++ db/db_test.js
@@ -0,0 +1,11 @@
+const db = require("./db_connection");
+
+// Add query request
+db.execute('Select 1 + 1 AS solution',
+    (error, results) => {
+        if (error)
+            throw error;
+        console.log(results);
+    }
+);
+
